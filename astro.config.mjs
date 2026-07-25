685affa8380b43b8e1caa9de1bab511a47fc6ae5
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-07-26 09:52:09 +1000

    initial blog setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://l3onwu.github.io',
+  base: '/blog',
+});
